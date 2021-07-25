b6bb568f92034165a2ca684cca94f9e3fca4e2cf
Leandro Cabral <enigmha@gmail.com>
2021-07-25 17:51:05 -0300

    H2 database

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,9 +2,19 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_category")
 public class Category implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
@@ -56,11 +66,4 @@ public class Category implements Serializable {
 			return false;
 		return true;
 	}
-	
-	
-	
-	
-	
-	
-
 }
