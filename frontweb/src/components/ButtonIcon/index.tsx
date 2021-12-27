8e4de491dceb80d5d09b3f096dec4a0d5f417d48
Leandro Cabral <enigmha@gmail.com>
2021-12-27 00:37:46 -0300

    Custom ButtonIcon

--- frontweb/src/components/ButtonIcon/index.tsx
+++ frontweb/src/components/ButtonIcon/index.tsx
@@ -1,11 +1,18 @@
 import './styles.css';
-const ButtonIcon = () => {
+import './styles.css';
+import { ReactComponent as SetaIcon } from 'assets/images/Seta.svg';
 
-    return(
-        <button className="btn btn-primary btn-icon">
-            <h6>INICIE AGORA A SUA BUSCA</h6>  
-        
-        </button>
-    );
-}
+const ButtonIcon = () => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>INICIE AGORA A SUA BUSCA</h6>
+      </button>
+
+      <div className="btn-icon-container">
+        <SetaIcon />
+      </div>
+    </div>
+  );
+};
 export default ButtonIcon;
